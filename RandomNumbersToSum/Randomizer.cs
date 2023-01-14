2160ecba3eeb7af5e681aff4ddfaf17bae97b3b5
Benjamin Rosenthal <brosen8690@gmail.com>
2023-01-14 10:42:09 -0500

    First draft without weighting

---
+++ RandomNumbersToSum/Randomizer.cs
@@ -0,0 +1,37 @@
+﻿namespace RandomNumbersToSum
+{
+    public class Randomizer
+    {
+        /// <summary>
+        /// Gets {count} numbers that, when summed, equal {sum}
+        /// </summary>
+        /// <param name="sum">The desired sum</param>
+        /// <param name="count">The number of random numbers to return</param>
+        /// <returns></returns>
+        public List<int> GetRandomNumbersEqualToSum(int sum, int count)
+        {
+
+            Random rand = new Random();
+            List<int> numbers = new List<int>();
+            int numberToAdd;
+            int remainder = sum;
+
+            for (int j = 1; j <= count; j++)
+            {
+                if (j == count)
+                {
+                    numberToAdd = remainder;
+                }
+                else
+                {
+                    numberToAdd = rand.Next(0, remainder);
+
+                }
+                remainder -= numberToAdd;
+                numbers.Add(numberToAdd);
+            }
+
+            return numbers;
+        }
+    }
+}
