56d8f1d3b6e0303624e9dcfffce6c5718ef0fa64
Benjamin Rosenthal <brosen8690@gmail.com>
2023-01-27 19:33:39 -0500

    trying a gaussian thing

--- RandomNumbersToSum/RandomizerExtensions.cs
+++ RandomNumbersToSum/RandomizerExtensions.cs
@@ -1,39 +1,68 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace RandomNumbersToSum
 {
     public static class RandomizerExtensions
     {
+        private static double nextNextGaussian;
+        private static bool haveNextNextGaussian = false;
+
+        public static double NextGaussian()
+        {
+            if (haveNextNextGaussian)
+            {
+                haveNextNextGaussian = false;
+                return nextNextGaussian;
+            }
+            else
+            {
+                Random random = new Random();
+
+                double v1, v2, s;
+                do
+                {
+                    v1 = 2 * random.NextDouble() - 1;   // between -1.0 and 1.0
+                    v2 = 2 * random.NextDouble() - 1;   // between -1.0 and 1.0
+                    s = v1 * v1 + v2 * v2;
+                } while (s >= 1 || s == 0);
+                double multiplier = Math.Sqrt(-2 * Math.Log(s) / s);
+                nextNextGaussian = v2 * multiplier;
+                haveNextNextGaussian = true;
+                return v1 * multiplier;
+            }
+        }
         /// <summary>
         /// Gets {count} numbers that, when summed, equal {sum}
         /// </summary>
         /// <param name="sum">The desired sum</param>
         /// <param name="count">The number of random numbers to return</param>
         /// <returns></returns>
-        public static List<int> ToRandomNumbers(this int sum, int count)
+        public static int[] ToRandomNumbers(this int sum, int count)
         {
-            Random rand = new Random();
-            List<int> numbers = new List<int>();
-            int numberToAdd;
-            int remainder = sum;
-
-            for (int j = 1; j <= count; j++)
+            if (count <= 0)
             {
-                if (j == count)
-                {
-                    numbers.Add(remainder);
-                    break;
-                }
-                else
-                {
-                    numberToAdd = rand.Next(0, remainder);
-
-                }
-                remainder -= numberToAdd;
-                numbers.Add(numberToAdd);
+                throw new ArgumentException("Count should be greater than 0.");
             }
+            Random random = new Random();
+            int[] integers = new int[count];
+            int remainingTotal = sum;
 
-            return numbers;
+            for (int i = 0; i < count - 1; i++)
+            {
+                double gaussianRandom = NextGaussian() + sum / count;
+                int randomValue = Convert.ToInt32(gaussianRandom);
+                if (randomValue < 1)
+                {
+                    randomValue = 1;
+                }
+
+                integers[i] = randomValue;
+                remainingTotal -= integers[i];
+            }
+            integers[count - 1] = remainingTotal;
+            return integers;
         }
 
         public static double GetBaseNumber(int remainder)
--- RandomNumbersToSumExecutable/Program.cs
+++ RandomNumbersToSumExecutable/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RandomNumbersToSum;
+using System.Linq;
 
-int remainder = 10000;
-Console.WriteLine(RandomizerExtensions.GetBaseNumber(remainder));
+//Console.WriteLine(RandomizerExtensions.GetBaseNumber(remainder));
 //Console.WriteLine(Math.Pow(RandomizerExtensions.GetBaseNumber(remainder), remainder));
+for (int i = 0; i < 1000; i++)
+{
+    10000.ToRandomNumbers(3).ToList().ForEach(x => Console.Write(x + " "));
+    Console.WriteLine();
+}
+var answer = 100.ToRandomNumbers(3);
+Console.ReadKey();
